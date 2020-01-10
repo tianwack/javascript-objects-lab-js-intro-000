9df6701d3696a085ee901d222dd18570ad804819
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 17:22:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
 
+updateObjectWithKeyAndValue(object, 'prop2', 2)
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
