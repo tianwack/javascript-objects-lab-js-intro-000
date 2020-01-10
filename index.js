be9a0af5c246275b09ee133c6eb8d2d0a9bb44e3
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 17:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  obj[key] = value
-  return obj
+  object[key] = value
+  return object
 }
