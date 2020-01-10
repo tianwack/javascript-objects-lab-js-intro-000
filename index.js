e5dd72fd5bddad1be1dee5138ff08da2db09d981
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 17:30:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromObjectByKey(object, key){
 deleteFromObjectByKey(newObj, prop);
 
 function destructivelyDeleteFromObjectByKey(object,key){
-  delete object.key
+  delete object[key]
   return object
 }
